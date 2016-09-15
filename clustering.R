f2d523e14d69ad7dde3830f9a2562661e69f264a
vbeakovic <valter.beakovic@gmail.com>
2016-09-15 20:52:03 +0200

    Plot showing clusters.

--- clustering.R
+++ clustering.R
@@ -1,4 +1,5 @@
 #### Libraries ####
+library(ggplot2)
 #### Data ####
 n = 100
 g = 6 
@@ -7,5 +8,7 @@ d <- data.frame(x = unlist(lapply(1:g, function(i) rnorm(n/g, runif(1)*i^2))),
                 y = unlist(lapply(1:g, function(i) rnorm(n/g, runif(1)*i^2))))
 
 #### First plot ####
+ggplot(d, aes(x = x, y = y), color = "blue") + 
+        geom_point()
 
 
