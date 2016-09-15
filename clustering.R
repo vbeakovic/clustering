dad6861e6c4ae16b9432d27ae272d049c03a5b76
vbeakovic <valter.beakovic@gmail.com>
2016-09-15 20:44:08 +0200

    First commit

---
+++ clustering.R
@@ -0,0 +1,11 @@
+#### Libraries ####
+#### Data ####
+n = 100
+g = 6 
+set.seed(g)
+d <- data.frame(x = unlist(lapply(1:g, function(i) rnorm(n/g, runif(1)*i^2))), 
+                y = unlist(lapply(1:g, function(i) rnorm(n/g, runif(1)*i^2))))
+
+#### First plot ####
+
+
